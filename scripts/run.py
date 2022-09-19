b74f88a20b83f42ded5b9bb665157d4a411db0ad
Võ Trần Hải Anh <v.anhvth8@vinai.io>
2022-09-19 07:20:06 +0000

    update

--- scripts/run.py
+++ scripts/run.py
@@ -20,19 +20,19 @@ for i in range(0, len(cmds), int(args.totalgpu)):
     
     _cmds = cmds[i:i+int(args.totalgpu)]
 
-    _cmds = " || ".join(_cmds)
+    _cmds = "\n".join(_cmds)
     
     
     tmpsh = f'/tmp/script-{wi}.sh'
     with open(tmpsh, 'w') as f:
         f.write(_cmds)
-
+    gpu = wi%8
     if i == 0:
         target_tmux = "run-0"
-        tmuxcmd = f"tmux new -s '{target_tmux}' -d 'sh {tmpsh} || echo Done sleep 300 && sleep 300'"
+        tmuxcmd = f"tmux new -s '{target_tmux}' -d 'CUDA_VISIBLE_DEVICES={gpu} sh {tmpsh} || echo Done sleep 300 && sleep 300'"
         
     else:
-        tmuxcmd = f"tmux new-window -n w{wi} -t {target_tmux}: 'sh {tmpsh} || echo Done Sleep 300 && sleep 300'"
+        tmuxcmd = f"tmux new-window -n w{wi} -t {target_tmux}: 'CUDA_VISIBLE_DEVICES={gpu} sh {tmpsh} || echo Done Sleep 300 && sleep 300'"
     wi += 1
     print(tmuxcmd)
-    # os.system(tmuxcmd)
+    os.system(tmuxcmd)
